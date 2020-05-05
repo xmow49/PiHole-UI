89f7219684b389641f5798976048c95922c912fd
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-05 16:37:44 +0200

    Create dual.py

---
+++ dual.py
@@ -0,0 +1,265 @@
+#!/usr/bin/python3
+
+import os
+import platform
+import time
+
+import humanize
+import psutil
+import requests
+
+#imports for Fritz.Box
+from fritzconnection import FritzConnection
+from fritzconnection.lib.fritzstatus import FritzStatus
+
+#imports for Display
+from luma.core.interface.serial import i2c
+from luma.core.sprite_system import framerate_regulator
+from luma.oled.device import ssd1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageSequence
+from datetime import datetime
+
+
+# Network interface to retrieve the IP address / use wlan0 for pizero / eth0 for pi
+interface = os.getenv('PIHOLE_OLED_INTERFACE', 'wlan0')
+# Mount point for disk usage info
+mount_point = os.getenv('PIHOLE_OLED_MOUNT_POINT', '/')
+# initialisation for Fritz.Box API / IP and Password may need to be changed.
+fritzconnection = FritzConnection(address='192.168.178.1', password='password')
+fc = FritzStatus(address='192.168.178.1', password='password')
+
+try:
+    serial = i2c(port=1, address=0x3C)
+    disp = ssd1306(serial)
+    is_noop = False
+except FileNotFoundError:
+    # The error is probably due to this script being run on a system that does
+    # not have an OLED connected. In this case, we create fake objects to
+    # render the result in the console.
+    from image_noop import NoopDisplay, NoopImage, InMemoryImageDraw
+
+    disp = NoopDisplay()
+    is_noop = True
+
+width = disp.width
+height = disp.height
+
+disp.clear()
+
+if is_noop:
+    image = NoopImage()
+    draw = InMemoryImageDraw(image)
+    font = None
+else:
+    image = Image.new('1', (width, height))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('./SF_Pixelate.ttf', 10)
+
+sleep = 1  # seconds
+
+hostname = platform.node()
+
+try:
+    elapsed_seconds = 0
+    while True:
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+        if elapsed_seconds == 20:
+            elapsed_seconds = 0
+
+        if elapsed_seconds >= 5 and elapsed_seconds <= 10:
+            addr = psutil.net_if_addrs()[interface][0]
+            draw.text(
+                (0, 0),
+                "Pi-hole %s" % addr.address.rjust(15),
+                font=font,
+                fill=255
+            )
+
+            uptime = datetime.now() - datetime.fromtimestamp(
+                psutil.boot_time()
+            )
+            
+            draw.text(
+                (0, 12),
+                "Up: %s" % humanize.naturaltime(uptime),
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (0, 22),
+                "    %.1f %.1f %.1f" % os.getloadavg(),
+                font=font,
+                fill=255
+            )
+
+            cpu = int(psutil.cpu_percent(percpu=False))
+            draw.text((0, 34), "CPU", font=font, fill=255)
+            draw.rectangle(
+                (26, 34, 126, 34 + 6),
+                outline=255,
+                fill=0
+            )
+            
+            draw.rectangle(
+               (26, 34, 26 + cpu, 34 + 6),
+                outline=255,
+                fill=255
+            )
+
+            mem = int(psutil.virtual_memory().percent)
+            draw.text((0, 44), "RAM", font=font, fill=255)
+            draw.rectangle(
+                (26, 44, 126, 44 + 6),
+                outline=255,
+                fill=0
+            )
+            
+            draw.rectangle(
+                (26, 44, 26 + cpu, 44 + 6),
+                outline=255,
+                fill=255
+            )
+
+            disk = int(psutil.disk_usage(mount_point).percent)
+            draw.text((0, 54), "Disk", font=font, fill=255)
+            draw.rectangle(
+                (26, 54, 126, 54 + 6),
+                outline=255,
+                fill=0
+            )
+            
+            draw.rectangle(
+                (26, 54, 26 + disk, 54 + 6),
+                outline=255,
+                fill=255
+            )
+            
+        elif elapsed_seconds >= 10 and elapsed_seconds <= 15:
+            fbuptime = fc.str_uptime
+            fbspeed = fc.str_max_bit_rate
+
+            draw.text(
+                (0, 0),
+                "Fritz.Box informations: ",
+                font=font,
+                fill=255
+                )
+
+            draw.line((0, 10, width, 10), fill=255)
+
+            draw.text(
+                (0, 14),
+                "Uptime: ",
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (64, 14),
+                fbuptime,
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (0,26),
+                "Upload-Speed: ",
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (50,36),
+                fbspeed[0],
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (0,46),
+                "Download-Speed: ",
+                font=font,
+                fill=255
+            )
+
+            draw.text(
+                (50,56),
+                fbspeed[1],
+                font=font,
+                fill=255
+            )
+
+        elif elapsed_seconds >= 15 and elapsed_seconds <= 20:
+            regulator = framerate_regulator(fps=10)
+            img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+            'res', 'code1.gif'))
+            code = Image.open(img_path)
+            size = [min(*device.size)] * 2
+            posn = ((device.width - size[0]) // 2, device.height - size[1])
+            
+            while True:
+                for frame in ImageSequence.Iterator(banana):
+                    with regulator:
+                        background = Image.new("RGB", device.size, "white")
+                        background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
+                        disp.display(background.convert("1"))
+            
+        else:
+            try:
+                req = requests.get('http://pi.hole/admin/api.php')
+                data = req.json()
+
+                draw.text(
+                    (0, 0),
+                    "Pi-hole (%s)" % data["status"],
+                    font=font,
+                    fill=255
+                )
+
+                draw.line((0, 12, width, 12), fill=255)
+
+                draw.text(
+                    (0, 22),
+                    "Blocked: %d (%d%%)" % (
+                        data["ads_blocked_today"],
+                        data["ads_percentage_today"]
+                    ),
+                    font=font,
+                    fill=255
+                )
+                
+                draw.text(
+                    (0, 32),
+                    "Queries: %d" % data["dns_queries_today"],
+                    font=font,
+                    fill=255
+                )
+
+                draw.line((0, 50, width, 50), fill=255)
+
+                draw.text(
+                    (0, 54),
+                    "Blocklist: %d" % data["domains_being_blocked"],
+                    font=font,
+                    fill=255
+                )
+                
+            except:  ## noqa
+                draw.text(
+                    (0, 0),
+                    "ERROR!",
+                    font=font,
+                    fill=255
+                )
+
+        disp.display(image)
+        time.sleep(sleep)
+
+        elapsed_seconds += 1
+except (KeyboardInterrupt, SystemExit):
+    print("Exiting...")
