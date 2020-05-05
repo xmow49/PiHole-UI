e843b00862f8d61a0b7a1ab6e0843f6345b15925
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-05 17:49:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,30 +9,33 @@ Displays an animated gif.
 """
 
 import os.path
-from demo_opts import get_device
+from luma.oled.device import ssd1306
+from luma.core.interface.serial import i2c
 from PIL import Image, ImageSequence
 from luma.core.sprite_system import framerate_regulator
 
+device0 = ssd1306(i2c(port=0, address=0x3C))
+device1 = ssd1306(i2c(port=1, address=0x3C))
 
 def main():
     regulator = framerate_regulator(fps=10)
-    img_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-        'images', 'banana.gif'))
-    banana = Image.open(img_path)
+    left_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+        'res', 'Fallin-L.gif'))
+    left = Image.open(img_path)
+    reight_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+        'res', 'Fallin-R.gif'))
+    right = Image.open(img_path)
     size = [min(*device.size)] * 2
     posn = ((device.width - size[0]) // 2, device.height - size[1])
 
     while True:
-        for frame in ImageSequence.Iterator(banana):
+        for frame in ImageSequence.Iterator(left):
             with regulator:
                 background = Image.new("RGB", device.size, "white")
                 background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
-                device.display(background.convert(device.mode))
-
-
-if __name__ == "__main__":
-    try:
-        device = get_device()
-        main()
-    except KeyboardInterrupt:
-        pass
+                device1.display(background.convert(device1.mode))
+        for frame in ImageSequence.Iterator(right):
+            with regulator:
+                background = Image.new("RGB", device.size, "white")
+                background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
+                device0.display(background.convert(device0.mode))
