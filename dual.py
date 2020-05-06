48ebc40cf93c5810c79ba1471e6d96141b8e8f1f
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-06 19:29:20 +0200

    Update dual.py

--- dual.py
+++ dual.py
@@ -3,10 +3,10 @@
 import os
 import platform
 import time
-
 import humanize
 import psutil
 import requests
+from threading import Thread
 
 #imports for Fritz.Box
 from fritzconnection import FritzConnection
@@ -41,7 +41,7 @@ disp2 = ssd1306(serial2)
 
 width = disp.width
 height = disp.height
-giftimer = 2
+disptimer = 0
 inforefreshtag = 1
 pindexleft = 0
 pindexright = 0
@@ -58,155 +58,141 @@ pindexright = 0
 
 #def addr():
 #    addr = psutil.net_if_addrs()[interface][0]
-
 #def uptime():
 #    uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
-    
 #def cpu():    
-#    cpu = int(psutil.cpu_percent(percpu=False))
-    
+#    cpu = int(psutil.cpu_percent(percpu=False))   
 #def mem():    
-#    mem = int(psutil.virtual_memory().percent)
-    
+#    mem = int(psutil.virtual_memory().percent)  
 #def disk():    
-#    disk = int(psutil.disk_usage(mount_point).percent)
-    
+#    disk = int(psutil.disk_usage(mount_point).percent) 
 #def fbuptime():    
-#    fbuptime = fc.str_uptime
-    
+#    fbuptime = fc.str_uptime  
 #def fbspeed():    
-#    fbspeed = fc.str_max_bit_rate
-    
+#    fbspeed = fc.str_max_bit_rate  
 #def req():    
 #    req = requests.get('http://pi.hole/admin/api.php')
 #     data = req.json()
 
 disp.clear()
+disp2.clear()
 
 image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()        #truetype('./SF_Pixelate.ttf', 10)
 
-sleep = 1  # seconds
+sleep = 4  # seconds
 
 hostname = platform.node()
 
-class LeftScreen():
-    def CMRScreen(
-
-try:
-    elapsed_seconds = 0
-    while True:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-        if elapsed_seconds == 17:
-            elapsed_seconds = 0
-
-class LeftScreen:
-    def LS1:           
+#class LeftScreen:
+def LS1:           
     #1st Screen CPU/RAM/Uptime..if elapsed_seconds >= 5 and elapsed_seconds <= 10: 
-        addr = psutil.net_if_addrs()[interface][0]
-        draw.text((0, 0), "Pi-hole %s" % addr.address.rjust(15), font=font, fill=255)
-        uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
-        draw.text((0, 12), "Up: %s" % humanize.naturaltime(uptime), font=font, fill=255)
-        draw.text((0, 22), "    %.1f %.1f %.1f" % os.getloadavg(), font=font, fill=255)
-        cpu = int(psutil.cpu_percent(percpu=False))
-        draw.text((0, 34), "CPU", font=font, fill=255)
-        draw.rectangle((26, 34, 126, 34 + 6), outline=255, fill=0)            
-        draw.rectangle((26, 34, 26 + cpu, 34 + 6), outline=255, fill=255)
-        mem = int(psutil.virtual_memory().percent)
-        draw.text((0, 44), "RAM", font=font, fill=255)
-        draw.rectangle((26, 44, 126, 44 + 6), outline=255, fill=0)
-        draw.rectangle((26, 44, 26 + cpu, 44 + 6), outline=255, fill=255)
-        disk = int(psutil.disk_usage(mount_point).percent)
-        draw.text((0, 54), "Disk", font=font, fill=255)
-        draw.rectangle((26, 54, 126, 54 + 6), outline=255, fill=0)            
-        draw.rectangle((26, 54, 26 + disk, 54 + 6), outline=255, fill=255 )
-        disp.display(image)
-    def LS2:
+   addr = psutil.net_if_addrs()[interface][0]
+   draw.text((0, 0), "Pi-hole %s" % addr.address.rjust(15), font=font, fill=255)
+   uptime = datetime.now() - datetime.fromtimestamp(psutil.boot_time())
+   draw.text((0, 12), "Up: %s" % humanize.naturaltime(uptime), font=font, fill=255)
+   draw.text((0, 22), "    %.1f %.1f %.1f" % os.getloadavg(), font=font, fill=255)
+   cpu = int(psutil.cpu_percent(percpu=False))
+   draw.text((0, 34), "CPU", font=font, fill=255)
+   draw.rectangle((26, 34, 126, 34 + 6), outline=255, fill=0)            
+   draw.rectangle((26, 34, 26 + cpu, 34 + 6), outline=255, fill=255)
+   mem = int(psutil.virtual_memory().percent)
+   draw.text((0, 44), "RAM", font=font, fill=255)
+   draw.rectangle((26, 44, 126, 44 + 6), outline=255, fill=0)
+   draw.rectangle((26, 44, 26 + cpu, 44 + 6), outline=255, fill=255)
+   disk = int(psutil.disk_usage(mount_point).percent)
+   draw.text((0, 54), "Disk", font=font, fill=255)
+   draw.rectangle((26, 54, 126, 54 + 6), outline=255, fill=0)            
+   draw.rectangle((26, 54, 26 + disk, 54 + 6), outline=255, fill=255 )
+   disp.display(image)
+def LS2:
     #2nd Screen PiHole Infos...
-        try:                                                
-            req = requests.get('http://pi.hole/admin/api.php')
-            data = req.json()
-            draw.text((0, 0), "Pi-hole (%s)" % data["status"], font=font, fill=255)
-            draw.line((0, 12, width, 12), fill=255)
-            draw.text((0, 22), "Blocked: %d (%d%%)" % (data["ads_blocked_today"], data["ads_percentage_today"]), font=font, fill=255)                
-            draw.text((0, 32), "Queries: %d" % data["dns_queries_today"], font=font, fill=255)
-            draw.line((0, 50, width, 50), fill=255)
-            draw.text((0, 54), "Blocklist: %d" % data["domains_being_blocked"], font=font, fill=255)                
-        except:
-            draw.text((0, 0), "ERROR!", font=font, fill=255)
-            disp.display(image)
+    try:                                                
+        req = requests.get('http://pi.hole/admin/api.php')
+        data = req.json()
+        draw.text((0, 0), "Pi-hole (%s)" % data["status"], font=font, fill=255)
+        draw.line((0, 12, width, 12), fill=255)
+        draw.text((0, 22), "Blocked: %d (%d%%)" % (data["ads_blocked_today"], data["ads_percentage_today"]), font=font, fill=255)                
+        draw.text((0, 32), "Queries: %d" % data["dns_queries_today"], font=font, fill=255)
+        draw.line((0, 50, width, 50), fill=255)
+        draw.text((0, 54), "Blocklist: %d" % data["domains_being_blocked"], font=font, fill=255)                
+    except:
+        draw.text((0, 0), "ERROR!", font=font, fill=255)
+        disp.display(image)
 
-class LeftScreen:
-    def RS1:                   
+#class LeftScreen:
+def RS1:                   
     #1st Fritzbox screen (uptime, up-/download) elapsed_seconds >= 10 and elapsed_seconds <= 15:
-        fbuptime = fc.str_uptime
-        fbspeed = fc.str_max_bit_rate
-        draw.text((0, 0), "Fritz.Box infos: ", font=font, fill=255)
-        draw.line((0, 10, width, 10), fill=255)
-        draw.text((0, 14), "Uptime: ", font=font, fill=255)
-        draw.text((64, 14), fbuptime, font=font, fill=255)
-        draw.text((0,26), "Upload-Speed: ", font=font, fill=255)
-        draw.text((50,36), fbspeed[0], font=font, fill=255)
-        draw.text((0,46), "Download-Speed: ", font=font, fill=255)
-        draw.text((50,56), fbspeed[1], font=font, fill=255)
-        disp2.display(image)
-    def RS2:
+    fbuptime = fc.str_uptime
+    fbspeed = fc.str_max_bit_rate
+    draw.text((0, 0), "Fritz.Box infos: ", font=font, fill=255)
+    draw.line((0, 10, width, 10), fill=255)
+    draw.text((0, 14), "Uptime: ", font=font, fill=255)
+    draw.text((64, 14), fbuptime, font=font, fill=255)
+    draw.text((0,26), "Upload-Speed: ", font=font, fill=255)
+    draw.text((50,36), fbspeed[0], font=font, fill=255)
+    draw.text((0,46), "Download-Speed: ", font=font, fill=255)
+    draw.text((50,56), fbspeed[1], font=font, fill=255)
+    disp2.display(image)
+def RS2:
     #2nd Fritzbox screen
-        hosts = fh.host_numbers
-        ssid = fw.ssid
-        missedcalls = fc.get_missed_calls(update=true, num=10, days=7)
-        draw.text((0, 0), "Fritz.Box infos: ", font=font, fill=255)
-        draw.line((0, 10, width, 10), fill=255)
-        draw.text((0, 14), "SSID: ", font=font, fill=255)
-        draw.text((64, 14), ssid, font=font, fill=255)
-        draw.text((0,26), "Hosts: ", font=font, fill=255)
-        draw.text((50,36), hosts, font=font, fill=255)
-        draw.text((0,46), "missed calls: ", font=font, fill=255)
-        draw.text((50,56), missedcalls, font=font, fill=255)
-        disp2.display(image)
+    hosts = fh.host_numbers
+    ssid = fw.ssid
+    missedcalls = fc.get_missed_calls(update=true, num=10, days=7)
+    draw.text((0, 0), "Fritz.Box infos: ", font=font, fill=255)
+    draw.line((0, 10, width, 10), fill=255)
+    draw.text((0, 14), "SSID: ", font=font, fill=255)
+    draw.text((64, 14), ssid, font=font, fill=255)
+    draw.text((0,26), "Hosts: ", font=font, fill=255)
+    draw.text((50,36), hosts, font=font, fill=255)
+    draw.text((0,46), "missed calls: ", font=font, fill=255)
+    draw.text((50,56), missedcalls, font=font, fill=255)
+    disp2.display(image)
         
-class GifLeft:
-        def LeftGif:
+#class GifLeft:
+def LeftGif:
         #Gifscreen for left display: elapsed_seconds >= 15 and elapsed_seconds <= 17:
-            regulator = framerate_regulator(fps=10)
-            left_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'res', 'Fallin-L.gif'))
-            left = Image.open(left_path)
-            size = [128, 64]
-            posn = (0, 0)
-            timecheck = time.time()            
-            while time.time() <= timecheck + giftimer:
-                 for frame in ImageSequence.Iterator(left):
-                    with regulator:
-                        background = Image.new("RGB", disp.size, "white")
-                        background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
-                        disp.display(background.convert("1"))
-                    if time.time() >= timecheck + giftimer:
-                       break
-
+    regulator = framerate_regulator(fps=10)
+    left_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'res', 'Fallin-L.gif'))
+    left = Image.open(left_path)
+    size = [128, 64]
+    posn = (0, 0)
+    timecheck = time.time()            
+    while time.time() <= timecheck + giftimer:
+         for frame in ImageSequence.Iterator(left):
+             with regulator:
+                  background = Image.new("RGB", disp.size, "white")
+                  background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
+                  disp.display(background.convert("1"))
+             if time.time() >= timecheck + giftimer:
+                  break
       
-class GifRight:
-        def RightGif:
+#class GifRight:
+def RightGif:
         #Gifscreen for right display: elapsed_seconds >= 15 and elapsed_seconds <= 17:
-            regulator2 = framerate_regulator(fps=10)
-            regulator2 = framerate_regulator(fps=10)
-            right_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'res', 'Fallin-R.gif'))
-            right = Image.open(right_path)
-            size = [128, 64]
-            posn = (0, 0)
-            timecheck = time.time()            
-            while time.time() <= timecheck + giftimer:
-                 for frame in ImageSequence.Iterator(right):
-                    with regulator:
-                        background = Image.new("RGB", disp.size, "white")
-                        background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
-                        disp2.display(background.convert("1"))
-                    if time.time() >= timecheck + giftimer:
-                       break
+    regulator2 = framerate_regulator(fps=10)
+    regulator2 = framerate_regulator(fps=10)
+    right_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'res', 'Fallin-R.gif'))
+    right = Image.open(right_path)
+    size = [128, 64]
+    posn = (0, 0)
+    timecheck = time.time()            
+    while time.time() <= timecheck + giftimer:
+         for frame in ImageSequence.Iterator(right):
+             with regulator:
+                 background = Image.new("RGB", disp.size, "white")
+                 background.paste(frame.resize(size, resample=Image.LANCZOS), posn)
+                 disp2.display(background.convert("1"))
+             if time.time() >= timecheck + giftimer:
+               break
 
-        #time.sleep(sleep)
+time.sleep(sleep)
 
-        #elapsed_seconds += 1
+if disptimer ==0 and disptimer <= 5:
+    screen1 = threading.Timer(5,  
+
+elapsed_seconds += 1
+
 except (KeyboardInterrupt, SystemExit):
     print("Exiting...")
