b6bc1aa16a5a758166c487e73013db7b1fb57e64
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-13 18:35:29 +0200

    Update HostChecker.py

--- modules/HostChecker.py
+++ modules/HostChecker.py
@@ -4,18 +4,30 @@ import os, sys
 SystemIP = '192.168.178.27'
 #192.168.178.27 is a sample, use any adress you wish.
 #the script will ping for it, if it's online -> Display on ->if not -> Display off
+PiHoleIP = 'pi.hole'
 
 def CheckIfUp():
     UPTag = ''
     response = os.system("ping -c 1 " + SystemIP)
-    print('System-Ping: ', response)
+    print('System-Ping: ', response)    
     if response == 0:
-        UPTag = 1
         f = open("/home/pi/PiHole-UI/modules/UPTag.txt", "w")
         f.write(str(UPTag))
         f.close()
     else:
-        UPTag = 0
         f = open("/home/pi/PiHole-UI/modules/UPTag.txt", "w")
+        f.write(str(UPTag))
+        f.close()
+
+def PiHoleUp():
+        UPTag = ''
+    response = os.system("ping -c 1 " + PiHoleIP)
+    print('System-Ping: ', response)    
+    if response == 0:
+        f = open("/home/pi/PiHole-UI/modules/PiHoleUp.txt", "w")
+        f.write(str(UPTag))
+        f.close()
+    else:
+        f = open("/home/pi/PiHole-UI/modules/PiHoleUp.txt", "w")
         f.write(str(UPTag))
         f.close()
