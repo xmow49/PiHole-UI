266a46213936c025f2444a060ca03e5417de3b11
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-14 16:18:19 +0200

    Update StatusLED.py

--- modules/StatusLED.py
+++ modules/StatusLED.py
@@ -12,9 +12,42 @@ GPIO.setup(22, GPIO.OUT)  #physical Pin-Nr.15 -> Indicator for CPU Usage above 6
 GPIO.setup(5, GPIO.OUT)   #physical Pin-Nr.33 -> Indicator for Fritzbox Online
 GPIO.setup(6, GPIO.OUT)   #physical Pin-Nr.35 -> Indicator for PiHole Service Running
 GPIO.setup(13, GPIO.OUT)  #physical Pin-Nr.37 -> Indicator for Host (HostChecker.py) online
+GPIO.setup(18, GPIO.OUT)  #physical Pin-Nr.12 -> Indicator for Pi is On
 
 CPUcheck = 1
 
+def SysStart():
+    GPIO.output(5, GPIO.LOW)
+    GPIO.output(6, GPIO.LOW)
+    GPIO.output(13, GPIO.LOW) 
+    GPIO.output(18, GPIO.LOW)
+    GPIO.setup(17, GPIO.LOW)
+    GPIO.setup(27, GPIO.LOW)
+    GPIO.setup(22, GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.output(5, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(6, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(13, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.setup(17, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.setup(27, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.setup(22, GPIO.HIGH)
+    time.sleep(1.)
+    GPIO.setup(22, GPIO.LOW)
+    GPIO.setup(13, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.setup(27, GPIO.LOW)
+    GPIO.setup(26, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.setup(17, GPIO.LOW)
+    GPIO.setup(5, GPIO.LOW)
+    
 def ProcessorLED():
     while CPUCheck == 1:
         picpu = int(psutil.cpu_percent(percpu=False))
