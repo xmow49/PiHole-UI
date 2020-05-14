988e40ae9855dfa24aec433b2c479a2e942ede5e
Maschine2501 <57831222+Maschine2501@users.noreply.github.com>
2020-05-14 17:19:31 +0200

    Update HostChecker.py

--- modules/HostChecker.py
+++ modules/HostChecker.py
@@ -2,15 +2,11 @@
 import os, sys
 import requests
 import time
-import RPi.GPIO as GPIO
 from fritzconnection.lib.fritzstatus import FritzStatus
+from modules.StatusLED import*
 
 #the script will ping for it, if it's online -> Display on ->if not -> Display off
 PiHoleIP = 'http://192.168.178.58/admin/api.php'
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(6, GPIO.OUT)   #physical Pin-Nr.35 -> Indicator for PiHole Service Running
 
 LoopTAG = 1
 
@@ -21,11 +17,15 @@ def CheckIfUp(SystemIP):
         response = os.system("ping -c 1 " + SystemIP)
         print('System-Ping: ', response)    
         if response == 0:
+           UPTag = 1
+           HostLEDon()
            f = open("/home/pi/PiHole-UI/modules/UPTag.txt", "w")
            f.write(str(UPTag))
            f.close()
            time.sleep(30.0)
         else:
+           UPTag = 0
+           HostLEDoff()
            f = open("/home/pi/PiHole-UI/modules/UPTag.txt", "w")
            f.write(str(UPTag))
            f.close()
@@ -37,10 +37,10 @@ def PiHoleUp():
         statuscode = response.status_code
         print('PI-Ping: ', response, 'statuscode: ', statuscode)
         if statuscode == 200:
-           GPIO.output(6, GPIO.HIGH)
+           PiHoleLEDon()
            time.sleep(60.0)
         else:
-           GPIO.output(6, GPIO.LOW)
+           PiHoleLEDoff()
            time.sleep(60.0)
       
 def FBconnected(FritzPW):
